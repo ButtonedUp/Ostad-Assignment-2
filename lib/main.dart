9f49203fd82e18f3bc0464117d53c82225ffdaa9
Nayon Datta <nayon.mr.5@gmail.com>
2024-11-15 22:34:37 +0600

    Ostad Assignment 2

--- lib/main.dart
+++ lib/main.dart
@@ -39,67 +39,66 @@ class Home extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
+      body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              //mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 100,
-                  backgroundColor: Colors.deepPurple.shade100,
-                  child: Icon(
-                    Icons.icecream_outlined,
-                    size: 100,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                //mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 100,
+                    backgroundColor: Colors.deepPurple.shade100,
+                    child: Icon(
+                      Icons.icecream_outlined,
+                      size: 100,
+                    ),
                   ),
-                ),
-                Text(
-                  "Ice cream is very delicious right?",
-                  style: TextStyle(fontSize: 20),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-            Column(
-              //mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 100,
-                  backgroundColor: Colors.deepPurple.shade100,
-                  child: Icon(
-                    Icons.code,
-                    size: 100,
+                  Text(
+                    "Ice cream is very delicious right?",
+                    style: TextStyle(fontSize: 20),
+                    textAlign: TextAlign.center,
                   ),
-                ),
-                Text(
-                  "Programming is not boring if you love it",
-                  style: TextStyle(fontSize: 20),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-            Column(
-              //mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 100,
-                  backgroundColor: Colors.deepPurple.shade100,
-                  child: Icon(
-                    Icons.egg,
-                    size: 100,
+                ],
+              ),
+              Column(
+                //mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 100,
+                    backgroundColor: Colors.deepPurple.shade100,
+                    child: Icon(
+                      Icons.code,
+                      size: 100,
+                    ),
                   ),
-                ),
-                Text(
-                  "If you submit code directly copy from chatgpt then mark will 0",
-                  style: TextStyle(fontSize: 20),
-                  textAlign: TextAlign.start,
-                ),
-              ],
-            ),
-          ],
-        ),
+                  Text(
+                    "Programming is not boring if you love it",
+                    style: TextStyle(fontSize: 20),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+              Column(
+                //mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 100,
+                    backgroundColor: Colors.deepPurple.shade100,
+                    child: Icon(
+                      Icons.egg,
+                      size: 100,
+                    ),
+                  ),
+                  Text(
+                    "If you submit code directly copy from chatgpt then mark will 0",
+                    style: TextStyle(fontSize: 20),
+                    textAlign: TextAlign.start,
+                  ),
+                ],
+              ),
+            ],
+          ),
       ),
     );
   }
