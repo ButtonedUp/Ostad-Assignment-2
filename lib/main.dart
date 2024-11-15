3b82367a99c10f28d5df69b47bf0888442e49566
Nayon Datta <nayon.mr.5@gmail.com>
2024-11-15 22:29:06 +0600

    Ostad Assignment 2

--- lib/main.dart
+++ lib/main.dart
@@ -22,79 +22,39 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My Profile'),
-        backgroundColor: Colors.yellow.shade600,
+        title: Text('Need Blood'),
+        backgroundColor: Colors.red,
+        centerTitle: true,
         actions: [
           IconButton(
             onPressed: () => {},
-            icon: Icon(Icons.add, color: Colors.black),
-          ),
-          IconButton(
-            onPressed: () => {},
-            icon: Icon(Icons.settings, color: Colors.black),
-          ),
-          IconButton(
-            onPressed: () => {},
-            icon: Icon(Icons.call, color: Colors.black),
+            icon: Icon(Icons.add, color: Colors.white),
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Column(
-              //mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 CircleAvatar(
                   radius: 100,
-                  backgroundColor: Colors.deepPurple.shade100,
+                  backgroundColor: Colors.grey.shade600,
                   child: Icon(
-                    Icons.icecream_outlined,
+                    Icons.bloodtype_outlined,
+                    color: Colors.red,
                     size: 100,
                   ),
                 ),
                 Text(
-                  "Ice cream is very delicious right?",
-                  style: TextStyle(fontSize: 20),
+                  "Donate Blood",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22,
+                  ),
                   textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-            Column(
-              //mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 100,
-                  backgroundColor: Colors.deepPurple.shade100,
-                  child: Icon(
-                    Icons.code,
-                    size: 100,
-                  ),
-                ),
-                Text(
-                  "Programming is not boring if you love it",
-                  style: TextStyle(fontSize: 20),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-            Column(
-              //mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 100,
-                  backgroundColor: Colors.deepPurple.shade100,
-                  child: Icon(
-                    Icons.egg,
-                    size: 100,
-                  ),
-                ),
-                Text(
-                  "If you submit code directly copy from chatgpt then mark will 0",
-                  style: TextStyle(fontSize: 20),
-                  textAlign: TextAlign.start,
                 ),
               ],
             ),
@@ -104,4 +64,3 @@ class Home extends StatelessWidget {
     );
   }
 }
-
